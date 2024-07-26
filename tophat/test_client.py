3bbbd5b569120d4b5c60b8a4c5518e00f19c66c7
Evan Hellman <ehellman@iastate.edu>
2024-07-26 17:16:24 -0500

    Remove junk from test_client.py.

--- tophat/test_client.py
+++ tophat/test_client.py
@@ -4,11 +4,8 @@ from tophat.api.client import TopHatClient
 from tophat.devices.neopixel import RainbowCommand
 from tophat.devices.printer import PrintCommand
 
-import adafruit_blinka.board.raspberrypi.raspi_40pin as board
-
 client: TopHatClient = TopHatClient(Path('/srv/tophat/test.socket'))
 
 client.send_command(0x1, PrintCommand('HELLO WORLD'))
 
 client.send_command(0x2, RainbowCommand(15))
-board.SCL
