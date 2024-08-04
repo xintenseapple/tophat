ea8f48d129ed63b887493ba0181c68112f9e4d73
Evan Hellman <ehellman@iastate.edu>
2024-08-04 14:52:47 -0500

    Added _get_impl_builder method to PrinterDevice.

--- src/tophat/devices/printer/__init__.py
+++ src/tophat/devices/printer/__init__.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
 
 from time import sleep
-from typing import Any, Set, Type, final
+from typing import Any, Callable, Set, Type, final
 
-from typing_extensions import Self, override
+from typing_extensions import Concatenate, Self, override
 
-from tophat.api.device import AsyncCommand, Command, Device
+from tophat.api.device import AsyncCommand, Command, Device, DeviceExtraParams
 
 
 @final
@@ -19,6 +19,11 @@ class PrinterDevice(Device):
     @override
     def supported_commands(cls: Type[Self]) -> Set[Type[Command[Self, Any]]]:
         return {PrintCommand, }
+
+    @classmethod
+    @override
+    def _get_impl_builder(cls: Type[Self]) -> Callable[Concatenate[str, DeviceExtraParams], Self]:
+        return cls
 
 
 class PrintCommand(AsyncCommand[PrinterDevice]):
