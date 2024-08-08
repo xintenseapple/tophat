695c86f9c244c95b15424b0fe837b35bd2257d08
Evan Hellman <ehellman@iastate.edu>
2024-08-08 10:10:00 -0500

    Hat stuff Pt. 12.

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+python3 -m tophat.run_server &> tophat_server.log &
+
+sudo python3 -m tophat.devices.neopixel.server 12 38 &> neopixel_server.log &
+
+sleep 2
+
+docker run -it -v /srv/tophat/tophat.socket:/var/run/tophat/tophat.socket tophat:nfc_wrangler
--- src/tophat/run_server.py
+++ src/tophat/run_server.py
@@ -5,6 +5,8 @@ from pathlib import Path
 
 from adafruit_platformdetect import Detector
 
+from tophat.devices.digital_switch import DigitalSwitchDevice
+
 if Detector().board.any_raspberry_pi_40_pin:
     import adafruit_blinka.board.raspberrypi.raspi_40pin as board
 else:
@@ -14,21 +16,21 @@ else:
 from tophat.api.server import TopHatServer
 from tophat.devices.neopixel import NeopixelDeviceProxy
 from tophat.devices.nfc_reader import PN532Device
-from tophat.devices.printer import PrinterDevice
 
 
 def main() -> None:
     server = TopHatServer(Path('/srv/tophat/tophat.socket'))
-    printer_device: PrinterDevice = server.register_device(PrinterDevice,
-                                                           'printer')
-    neopixel_device: NeopixelDeviceProxy = server.register_device(NeopixelDeviceProxy,
-                                                                  'neopixels',
-                                                                  Path('/srv/tophat/neopixel.socket'))
-    nfc_device: PN532Device = server.register_device(PN532Device,
-                                                     'nfc_reader',
-                                                     board.SCK, board.MOSI, board.MISO, board.D25,
-                                                     server.manager)
-    nfc_device.start_reader()
+    server.register_device(NeopixelDeviceProxy,
+                           'neopixels',
+                           Path('/srv/tophat/neopixel.socket'))
+    server.register_device(PN532Device,
+                           'nfc_reader',
+                           board.SCK, board.MOSI, board.MISO, board.D25,
+                           server.manager).start_reader()
+    server.register_device(DigitalSwitchDevice,
+                           'headlamp',
+                           board.D23)
+
     server.start()
 
 
