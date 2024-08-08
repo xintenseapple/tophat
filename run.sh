6f5b24e02b67ad0040044f3428a0eff85f7dc3d2
Evan Hellman <ehellman@iastate.edu>
2024-08-08 11:05:46 -0500

    Added nfc_wrangler binary Pt. 2.

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ sudo python3 -m tophat.devices.neopixel.server 12 38 &> neopixel_server.log &
 
 sleep 2
 
-docker run -it -v /srv/tophat/tophat.socket:/var/run/tophat/tophat.socket tophat:nfc_wrangler
+docker run -v /srv/tophat/tophat.socket:/var/run/tophat/tophat.socket tophat:nfc_wrangler &> nfc_wrangler.log &
