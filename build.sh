d925b038a631df9ae321a34ff5b7d951ece6d5ae
Evan Hellman <ehellman@iastate.edu>
2024-08-09 01:30:50 -0500

    Remove pip force reinstall in build.sh.

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ docker image rm -f tophat:base tophat:nfc_wrangler tophat:heap_miner
 
 git pull && git submodule update
 
-sudo python3 -m pip install --force-reinstall --break-system-packages ./
+sudo python3 -m pip install --break-system-packages ./
 
 docker build -t tophat:base ./
 docker build -t tophat:nfc_wrangler ./src/tophat/hats/nfc_wrangler
